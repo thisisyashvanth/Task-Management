631571e5627b954062dbe9852c46089613ca541a
Yashvanth <yashvanth.vijayabalaji@acxhange.com>
2026-03-05 10:48:42 +0530

    Get Endpoint Working

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from sqlalchemy import engine
-
+from core.database import engine
+from api.routes.taskRouter import taskRouter
 from core.database import Base
 
 @asynccontextmanager
@@ -10,4 +10,6 @@ async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
     yield
 
-app = FastAPI(title="Task App")
+app = FastAPI(title="Task App", lifespan=lifespan)
+
+app.include_router(taskRouter)
